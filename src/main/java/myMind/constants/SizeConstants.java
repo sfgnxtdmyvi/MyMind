f9eb3d3bd89b5a199ce77c7522d3b6aefbbfb237
zhenyk <zhenyk57890@hundsun.com>
2026-04-13 15:06:08 +0800

    暂时完成MindNode高度调整逻辑，高度过大时，仍可能出现问题，介高度为整个屏幕时，仍正常显示

--- src/main/java/myMind/constants/SizeConstants.java
+++ src/main/java/myMind/constants/SizeConstants.java
@@ -1,11 +1,17 @@
 package myMind.constants;
 
 public class SizeConstants {
-    public static final int MIN_NODE_WIDTH = 125;
-    public static final int MAX_NODE_WIDTH = 722;
-    public static final int MIN_NODE_HEIGHT = 52;
-    public static final int NODE_GAP_X = MIN_NODE_WIDTH / 4;
-    public static final int NODE_GAP_Y = MIN_NODE_WIDTH / 4;
-    public static final int NODE_FONT_SIZE = 20;
+	public static final int MIN_NODE_WIDTH = 125;
+	public static final int MAX_NODE_WIDTH = 722;
+	public static final int MIN_NODE_HEIGHT = 50;
+
+	public static final int MIN_TEXTAREA_WIDTH = MIN_NODE_WIDTH - 22;
+	public static final int MIN_TEXTAREA_HEIGHT = MIN_NODE_HEIGHT - 22;
+
+
+	public static final int NODE_GAP_X = MIN_NODE_WIDTH / 4;
+	public static final int NODE_GAP_Y = MIN_NODE_WIDTH / 5;
+
+	public static final int NODE_FONT_SIZE = 20;
 
 }
