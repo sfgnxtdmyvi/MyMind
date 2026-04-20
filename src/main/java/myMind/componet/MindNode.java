2cca1b3d35d9bf42907411e49d7228d04b1a864b
“chef” <k825566@163.com>
2026-04-20 23:24:21 +0800

    1.图片边框 2.图片缩放

--- src/main/java/myMind/componet/MindNode.java
+++ src/main/java/myMind/componet/MindNode.java
@@ -4,11 +4,14 @@ import javafx.application.Platform;
 import javafx.embed.swing.SwingFXUtils;
 import javafx.geometry.Insets;
 import javafx.geometry.Pos;
+import javafx.scene.Cursor;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import javafx.scene.input.KeyCode;
 import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.Priority;
+import javafx.scene.layout.Region;
+import javafx.scene.layout.StackPane;
 import javafx.scene.layout.VBox;
 import javafx.scene.text.Font;
 import javafx.scene.text.Text;
@@ -37,12 +40,19 @@ public class MindNode extends VBox {
     private final NodeController controller;
     //用于测量文本尺寸
     private Text measureText;
-    private ImageView image; // 新增图片视图
+    private final StackPane imageContainer;
+    private ImageView image;
 
     // 拖拽缩放相关变量
-    private static final double RESIZE_THRESHOLD = 5.0;
+    private static final double RESIZE_THRESHOLD = 8.0;
     private boolean isResizing = false;
-    private double startX, startY, startWidth, startHeight;
+    private double startX;
+    private double startY;
+    private double startWidth;
+    private double startHeight;
+    private double minImageWidth;
+    private double minImageHeight;
+    private double ratio;
 
     public MindNode(NodeModel model, NodeController controller) {
         this.model = model;
@@ -56,15 +66,22 @@ public class MindNode extends VBox {
         //当改变宽度或高度时，另一个维度会自动按比例缩放
         image.setPreserveRatio(true);
         image.setSmooth(true);
-        image.setVisible(false);
         //true：组件会参与布局计算
         //false：组件脱离布局管理
         image.setManaged(false);
 
+        // Pane 负责显示边框
+        // 只有 Region 及其子类才能通过 CSS 设置边框和背景
+        imageContainer = new StackPane(image);
+        // 在一个会拉伸子节点的布局容器中，如果子节点没有设置最大尺寸限制，它会填满可用空间
+        imageContainer.setMaxSize(Region.USE_PREF_SIZE, Region.USE_PREF_SIZE);
+        imageContainer.setVisible(false);
+        imageContainer.setManaged(false);
+
         textArea = new InlineCssTextArea();
         textArea.replaceText(0, 0, model.getText());
         textArea.setWrapText(true);
-        textArea.getStyleClass().add("nodeContent");
+        textArea.getStyleClass().add("nodeTextArea");
         // 让绘制连线时，能获取节点位置
         textArea.setPrefWidth(SizeConstants.MIN_TEXTAREA_WIDTH);
         textArea.setPrefHeight(SizeConstants.MIN_TEXTAREA_HEIGHT);
@@ -75,7 +92,7 @@ public class MindNode extends VBox {
         getStyleClass().add("nodeBorder");
         setPadding(new Insets(10, 10, 10, 10));
 
-        getChildren().addAll(image, textArea);
+        getChildren().addAll(imageContainer, textArea);
         VBox.setVgrow(textArea, Priority.ALWAYS);
 
         // 模型x、y变化时，改变位置
@@ -96,7 +113,7 @@ public class MindNode extends VBox {
 
         // 文本变化动态调整
         textArea.textProperty().addListener((obs, oldText, newText) -> {
-            Platform.runLater(this::adjustSizeR);
+            Platform.runLater(this::adjustSize);
         });
 
         // 粘贴图片
@@ -108,70 +125,89 @@ public class MindNode extends VBox {
                     try {
                         BufferedImage bufferedImage = (BufferedImage) transferable.getTransferData(DataFlavor.imageFlavor);
                         Image clipboardImage = SwingFXUtils.toFXImage(bufferedImage, null);
+
+                        //如果开启了 150% 缩放
+                        //截图时，系统记录的是逻辑像素，比如 100x100，按 150% 缩放渲染出来是 150x150
+                        //但 awt 剪贴析拿到的是物理像素，就是 150x150，再按 150% 缩放渲染出来是 225x225
+                        double scale = this.getScene().getWindow().getOutputScaleX();
                         image.setImage(clipboardImage);
-//                        saveImage(bufferedImage);
+                        double width = clipboardImage.getWidth() / scale;
+                        double height = clipboardImage.getHeight() / scale;
+                        minImageWidth = width / 2;
+                        minImageHeight = height / 2;
+                        image.setFitWidth(width);
+                        image.setFitHeight(height);
+                        ratio = minImageWidth / minImageHeight;
+
+                        imageContainer.setVisible(true);
+                        imageContainer.setManaged(true);
+                        //                        saveImage(bufferedImage);
                     } catch (UnsupportedFlavorException | IOException ex) {
                         ex.printStackTrace();
                     }
-                    image.setFitWidth(100);
-                    image.setFitHeight(100);
                     image.setVisible(true);
                     image.setManaged(true);
-                    adjustSizeR();
+                    adjustSize();
                 }
 
                 e.consume();
             }
         });
 
-        // --- 图片缩放逻辑 ---
-//        image.setOnMousePressed(e -> {
-//            if (image.isVisible()) {
-//                startX = e.getSceneX();
-//                startY = e.getSceneY();
-//                startWidth = image.getFitWidth();
-//                startHeight = image.getFitHeight();
-//
-//                // 判断是否点击在右下角区域用于缩放
-//                if (e.getX() > image.getBoundsInLocal().getWidth() - RESIZE_THRESHOLD &&
-//                        e.getY() > image.getBoundsInLocal().getHeight() - RESIZE_THRESHOLD) {
-//                    isResizing = true;
-//                    image.setCursor(Cursor.SE_RESIZE);
-//                }
-//            }
-//        });
-//
-//        image.setOnMouseDragged(e -> {
-//            if (isResizing) {
-//                double deltaX = e.getSceneX() - startX;
-//                double deltaY = e.getSceneY() - startY;
-//
-//                // 简单的等比例缩放或自由缩放，这里演示自由缩放
-//                double newWidth = Math.max(20, startWidth + deltaX);
-//                double newHeight = Math.max(20, startHeight + deltaY);
-//
-//                image.setFitWidth(newWidth);
-//                image.setFitHeight(newHeight);
-//
-//                // 图片大小改变后，需要重新计算节点整体尺寸
-//                adjustSizeR();
-//            }
-//        });
-//
-//        image.setOnMouseReleased(e -> {
-//            isResizing = false;
-//            image.setCursor(Cursor.DEFAULT);
-//        });
-//
-//        // 鼠标悬停在右下角显示缩放光标
-//        image.setOnMouseMoved(e -> {
-//            if (e.getX() > image.getBoundsInLocal().getWidth() - RESIZE_THRESHOLD &&
-//                    e.getY() > image.getBoundsInLocal().getHeight() - RESIZE_THRESHOLD) {
-//                image.setCursor(Cursor.SE_RESIZE);
-//            } else {
-//                image.setCursor(Cursor.DEFAULT);
-//            }
-//        });
+        // --- 图片缩放 ---
+        // 鼠标移入时，显示边框
+        imageContainer.setOnMouseEntered(e -> imageContainer.getStyleClass().add("nodeImage"));
+
+        imageContainer.setOnMouseExited(e -> {
+            if (!isResizing) {
+                imageContainer.getStyleClass().remove("nodeImage");
+            }
+        });
+
+        // 在右下角显示缩放图标
+        imageContainer.setOnMouseMoved(e -> {
+            if (e.getX() > image.getBoundsInLocal().getWidth() - RESIZE_THRESHOLD
+                    && e.getY() > image.getBoundsInLocal().getHeight() - RESIZE_THRESHOLD) {
+                imageContainer.setCursor(Cursor.SE_RESIZE);
+            } else {
+                imageContainer.setCursor(Cursor.DEFAULT);
+            }
+        });
+
+        imageContainer.setOnMousePressed(e -> {
+            if (image.isVisible()) {
+                startX = e.getSceneX();
+                startY = e.getSceneY();
+                startWidth = image.getFitWidth();
+                startHeight = image.getFitHeight();
+
+                if (e.getX() > image.getBoundsInLocal().getWidth() - RESIZE_THRESHOLD
+                        && e.getY() > image.getBoundsInLocal().getHeight() - RESIZE_THRESHOLD) {
+                    isResizing = true;
+                    image.setCursor(Cursor.SE_RESIZE);
+                }
+            }
+        });
+
+        imageContainer.setOnMouseDragged(e -> {
+            if (isResizing) {
+                double deltaX = e.getSceneX() - startX;
+
+                // 根据宽度的变化量，按宽高比计算高度
+                double newWidth = Math.max(minImageWidth, startWidth + deltaX);
+                double newHeight = Math.max(minImageHeight, startHeight + deltaX / ratio);
+
+                image.setFitWidth(newWidth);
+                image.setFitHeight(newHeight);
+
+                adjustSize();
+            }
+        });
+
+        imageContainer.setOnMouseReleased(e -> {
+            isResizing = false;
+            image.setCursor(Cursor.DEFAULT);
+        });
     }
 
     private static void saveImage(BufferedImage bufferedImage) {
@@ -186,9 +222,10 @@ public class MindNode extends VBox {
     /**
      * 根据内容动态调整尺寸
      */
-    private void adjustSizeR() {
+    private void adjustSize() {
         String text = textArea.getText();
-        if (!image.isVisible() && text.isEmpty()) {
+        boolean imageVisible = image.isVisible();
+        if (!imageVisible && text.isEmpty()) {
             textArea.setPrefWidth(SizeConstants.MIN_TEXTAREA_WIDTH);
             textArea.setPrefHeight(SizeConstants.MIN_TEXTAREA_HEIGHT);
             setPrefWidth(SizeConstants.MIN_NODE_WIDTH);
@@ -206,6 +243,9 @@ public class MindNode extends VBox {
         // MIN_NODE_WIDTH <= 宽度 <= MAX_NODE_WIDTH
         nodeWidth = Math.max(SizeConstants.MIN_NODE_WIDTH,
                 Math.min(nodeWidth, SizeConstants.MAX_NODE_WIDTH));
+        if (imageVisible) {
+            nodeWidth = Math.max(nodeWidth, image.getFitWidth()+24);
+        }
 
         // 设置换行
         measureText.setWrappingWidth(SizeConstants.MAX_NODE_WIDTH - 22);
@@ -214,8 +254,10 @@ public class MindNode extends VBox {
         double totalPadding = (contentHeight / 25.4) * 2.6;
         double textHeight = contentHeight + totalPadding;
         // MindNode 高度 = border(2px) + padding(20px) + image高度 + textArea 高度
-        double imageHeight = image.getFitHeight();
-        double nodeHeight = imageHeight + textHeight + 22;
+        double nodeHeight = textHeight + 22;
+        if (imageVisible) {
+            nodeHeight += image.getFitHeight() + 2;
+        }
 
         // y轴 - 高度变动的一半，让中心保持不变
         if (model.getPos() == PosConstants.MIDDLE) {
@@ -242,9 +284,12 @@ public class MindNode extends VBox {
         if (children.isEmpty()) {
             return;
         }
+        System.out.println("getFitWidth = " + image.getFitWidth());
+
 
         double parentX = nodeModel.getX();
         double parentWidth = nodeModel.getSelfWidth();
+        System.out.println("parentWidth = " + parentWidth);
         double childX = parentX + parentWidth + SizeConstants.NODE_GAP_X;
 
         for (NodeModel child : children) {
